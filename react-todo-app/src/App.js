36de028f4461573ac65370176cc59aa0d2535e29
Collin Porter <colls.potta@gmail.com>
2022-12-20 15:24:38 -0700

    install axios & use axios to get todo data in console

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const todosURL = "https://jsonplaceholder.typicode.com/todos";
+
+  useEffect(() => {
+    axios.get(todosURL).then((res) => {
+      console.log(res.data);
+      setTodos(res.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Hello World</header>
     </div>
   );
 }
