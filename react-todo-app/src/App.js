161f4c3bfa1b308c78f023b812a17fa357bc2bd8
Collin Porter <colls.potta@gmail.com>
2022-12-21 13:55:09 -0700

    get todos on page

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -5,19 +5,41 @@ import axios from "axios";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
   const todosURL = "https://jsonplaceholder.typicode.com/todos";
 
+  const headerStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Arial",
+    fontSize: "32px",
+    display: "flex",
+    justifyContent: "center",
+  };
+
+  // AXIOS GET REQUEST
   useEffect(() => {
     axios.get(todosURL).then((res) => {
-      console.log(res.data);
-      setTodos(res.data);
+      console.table(res.data);
+      const allTodos = res.data;
+      setTodos(allTodos);
     });
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">Hello World</header>
-    </div>
+    <>
+      <header style={headerStyle}>TODO APP</header>
+      <div className="todo-wrapper">
+        {todos.map((todo) => {
+          return (
+            <div key={todo.id}>
+              <div>{todo.title}</div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
