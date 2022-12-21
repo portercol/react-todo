9c75b4caeb3d2d0939c96a89ee62c02bd5257d5e
Collin Porter <colls.potta@gmail.com>
2022-12-21 15:52:52 -0700

    removed unused files and imports

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import AddTodo from "./components/AddTodo";
-// import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState([]);
